7ff29314dde2b72809be948408c970caab39ad6e
Nico-dsa <6463@holbertonstudenst.com>
2024-01-24 12:48:14 +0100

    Try to Fix Sandpiles sum

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,92 +1,98 @@
-#include <stdio.h>
-#include <stdbool.h>
-
 #include "sandpiles.h"
 
-void print_sandpiles(int grid1[3][3], int grid2[3][3]);
-void print_grid(int grid[3][3]);
-bool is_stable(int grid[3][3]);
-void topple(int grid[3][3]);
+/**
+ * is_stable - checks if sandpile is stable.
+ * @grid: grid to check
+ * Return: 1 if stable else 0
+ */
 
-void sandpiles_sum(int grid1[3][3], int grid2[3][3]) {
-    // Afficher les sabliers avant la somme
-    print_sandpiles(grid1, grid2);
+int is_stable(int grid[3][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (grid[i][j] > 3)
+                return (0);
+        }
+    }
+    return (1);
+}
 
-    // Somme des sabliers
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
+/**
+ * topple - topples the sandpile to distribute sand
+ * @grid: grid to topple
+ */
+
+void topple(int grid[3][3])
+{
+    int tempGrid[3][3];
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            tempGrid[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (grid[i][j] > 3)
+            {
+                tempGrid[i][j] -= 4;
+
+                if (i - 1 >= 0)
+                    tempGrid[i - 1][j]++;
+                if (i + 1 < 3)
+                    tempGrid[i + 1][j]++;
+                if (j - 1 >= 0)
+                    tempGrid[i][j - 1]++;
+                if (j + 1 < 3)
+                    tempGrid[i][j + 1]++;
+            }
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            grid[i][j] += tempGrid[i][j];
+        }
+    }
+}
+
+/**
+ * sandpiles_sum - compute the sum of two sandpiles.
+ * @grid1: first sandpile
+ * @grid2: second sandpile
+ */
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
             grid1[i][j] += grid2[i][j];
         }
     }
-
-    // Afficher la grille après la somme
-    printf("=\n");
-    print_grid(grid1);
-
-    // Écrouler jusqu'à stabilisation
-    while (!is_stable(grid1)) {
+    while (!is_stable(grid1))
+    {
+        printf("=\n");
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (j)
+                    printf(" ");
+                printf("%d", grid1[i][j]);
+            }
+            printf("\n");
+        }
         topple(grid1);
-        print_grid(grid1);
-    }
-}
-
-void print_sandpiles(int grid1[3][3], int grid2[3][3]) {
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
-            printf("%d ", grid1[i][j]);
-        }
-        if (i == 1) {
-            printf("+ ");
-        } else {
-            printf("  ");
-        }
-        for (int j = 0; j < 3; ++j) {
-            printf("%d ", grid2[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void print_grid(int grid[3][3]) {
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
-            printf("%d ", grid[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-}
-
-bool is_stable(int grid[3][3]) {
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
-            if (grid[i][j] > 3) {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-void topple(int grid[3][3]) {
-    int temp[3][3] = {0};
-
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
-            if (grid[i][j] > 3) {
-                temp[i][j] -= 4;
-                if (i > 0) temp[i - 1][j]++; // Up
-                if (i < 2) temp[i + 1][j]++; // Down
-                if (j > 0) temp[i][j - 1]++; // Left
-                if (j < 2) temp[i][j + 1]++; // Right
-            }
-        }
-    }
-
-    // Apply the toppling
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
-            grid[i][j] += temp[i][j];
-        }
     }
 }
